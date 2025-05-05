623a94c6312776182b8c1e705b3cf92d70ccf1eb
bnunnagoppula <bnunnagoppula@rvohealth.com>
2025-05-05 15:56:14 +0530

    use of backticks - interpolation

--- javascript_learning/ClassTest.js
+++ javascript_learning/ClassTest.js
@@ -9,5 +9,6 @@ class Employee {
 emp1 = new Employee("Nicholas", 1005);
 emp2 = new Employee("Sheldon", 1050);
 
-console.log(emp1.name + " - " + emp1.id);
-console.log(emp2.name + " - " + emp2.id);
+// console.log(emp1.name + " - " + emp1.id);
+// console.log(emp2.name + " - " + emp2.id);
+console.log(`${emp1.name} and ${emp2.name}`);
